8de6e0faa9ab30f1d8d22197571d91eab3464441
rabarbra98-cloud <rabarbra98@gmail.com>
2026-04-16 14:09:30 +0200

    Did some animations, finished for hand-in

--- script.js
+++ script.js
@@ -10,17 +10,20 @@ console.log(todoList);
 function deleteTaskButton (event) {
   // console.log("Button pressed")
   // console.log(event)
-  event.target.parentElement.parentElement.remove()
+  event.target.parentElement.parentElement.classList.add("slideOut")
+  setTimeout(function(){event.target.parentElement.parentElement.remove()},1000)
 }
 
 function doneTaskButton (event) {
-  console.log(event.target.parentElement.parentElement.classList.toggle("strikethrough"))
+  event.target.parentElement.parentElement.classList.toggle("strikethrough")
 }
 
 submitButton.addEventListener('click', function () {
+  if (todoInput.value == "") {console.log("empty");}
+  else{
   const taskObject = document.createElement('li');
-  const taskButtonDone = document.createElement("button");
-  const taskButtonDelete = document.createElement("button");
+  const taskButtonDone = document.createElement("button");  //Kunne spart mange linjer med kode her hvis jeg visste
+  const taskButtonDelete = document.createElement("button");//hvordan man genererer et objekt med spesifikke egenskaper
   const taskButtonContainer = document.createElement("span")
   taskButtonDone.textContent = "Ferdig"
   taskButtonDone.classList.add("doneButton")
@@ -28,15 +31,16 @@ submitButton.addEventListener('click', function () {
   taskButtonDelete.classList.add("deleteButton")
   taskButtonContainer.appendChild(taskButtonDone)
   taskButtonContainer.appendChild(taskButtonDelete)
-  console.log(taskButtonContainer)
+  // console.log(taskButtonContainer)
   
   taskObject.textContent = todoInput.value;
-  taskObject.classList.add('todoItem');
+  taskObject.classList.add('todoItem', 'growIn');
   taskObject.appendChild(taskButtonContainer)
   todoList.appendChild(taskObject);
-  console.log(todoInput.value);
+  // console.log(todoInput.value);
 
   taskButtonDelete.addEventListener("click", deleteTaskButton)
   taskButtonDone.addEventListener("click",doneTaskButton)
+}
   event.preventDefault();
 });
